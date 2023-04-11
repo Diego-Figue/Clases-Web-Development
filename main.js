9a1694e269f7a1d3d3b5275c3a655824697b5bc6
Diego-Figue <diegofigue0212@gmail.com>
2023-04-11 09:23:57 -0500

    add class 8

---
+++ main.js
@@ -0,0 +1,66 @@
+// Variables
+
+var jugador = 'Emiliano'
+
+//tipos de datos
+
+var string = 'Cadena de texto'
+
+var number = 2023
+
+var boolean = true // true o false
+
+var isUndefined = undefined
+
+var isNull = null
+
+//Case Sensitive
+
+var ceoEscalab = 'Cesar'
+
+var ceoescalab = 'Claudio'
+
+//estilos de escritura
+
+var userClickCount = 'camelCase'
+
+var UserClickCount = 'PascalCase'
+
+var user_click_count = 'snake_case'
+
+//objetos y arrays
+
+//objeto ejemplo
+var myCar = {
+    color: 'rojo',
+    marca: 'bmw',
+    modelo: 2019
+}
+
+//array ejemplo
+
+var estudiantes = ['Ivan','Daniel','Luis','Karen','Omar']
+
+
+//redefinir variables
+
+var ganador = 'Daniel'
+
+
+ganador = 007
+
+//no usar var, explicacion de hoisting
+
+
+
+const numeroOne = 3123123
+
+console.log(numeroOne)
+
+// numeroOne = 'uno'
+
+console.log(numeroOne)
+
+
+
+
