31007842a1e99995e14779e69d832304931b3928
Diego-Figue <diegofigue0212@gmail.com>
2023-04-20 08:34:04 -0500

    ADD: class 10

--- main.js
+++ main.js
@@ -78,74 +78,200 @@
 // Seleccionando el DOM
 
 // con getElementById
-const titulo = document.getElementById('titulo');
+// const titulo = document.getElementById('titulo');
 
-//con getelementByTagName
+// //con getelementByTagName
 
-// const itemsLi = document.getElementsByTagName('li')
-const itemsLi = document.getElementsByTagName('li')[2]
+// // const itemsLi = document.getElementsByTagName('li')
+// const itemsLi = document.getElementsByTagName('li')[2]
 
-//con getElementByClassName
+// //con getElementByClassName
 
-const byClass = document.getElementsByClassName('byClass')
+// const byClass = document.getElementsByClassName('byClass')
 
-// con querySelector
+// // con querySelector
 
-const qSelector = document.querySelector('div');
+// const qSelector = document.querySelector('div');
 
-// con querySelectorAll
+// // con querySelectorAll
 
-const qSelectorAll = document.querySelectorAll("a[href*='https://escalab.tech/']")
+// const qSelectorAll = document.querySelectorAll("a[href*='https://escalab.tech/']")
 
-//////////////////////////////////////////////////////////
+// //////////////////////////////////////////////////////////
 
-// EVENTOS
+// // EVENTOS
 
-const set = document.getElementById('set');
-const getItem = document.getElementById('get');
-const del = document.getElementById('delete');
+// const set = document.getElementById('set');
+// const getItem = document.getElementById('get');
+// const del = document.getElementById('delete');
 
-const cuadrado = document.getElementById('changeColor')
+// const cuadrado = document.getElementById('changeColor')
 
-const test = document.getElementById('test')
+// const test = document.getElementById('test')
 
-const escaladores = ['Ivan','Luis','Daniel','Emiliano']
-const year = 2023
+// const escaladores = ['Ivan','Luis','Daniel','Emiliano']
+// const year = 2023
 
-set.addEventListener('click', () => {
-    // cuadrado.style.backgroundColor = 'blue';
-    // cuadrado.style.width = '100px'
+// set.addEventListener('click', () => {
+//     // cuadrado.style.backgroundColor = 'blue';
+//     // cuadrado.style.width = '100px'
 
-    // cuadrado.classList.add('dark')
-    // cuadrado.classList.remove('green')
-    // cuadrado.classList.toggle('dark')
+//     // cuadrado.classList.add('dark')
+//     // cuadrado.classList.remove('green')
+//     // cuadrado.classList.toggle('dark')
 
-    // const titleSquare = document.createElement('h3')
+//     // const titleSquare = document.createElement('h3')
 
-    // titleSquare.textContent = 'Titulo Cuadrado'
+//     // titleSquare.textContent = 'Titulo Cuadrado'
 
-    // cuadrado.appendChild(titleSquare);
+//     // cuadrado.appendChild(titleSquare);
 
-    // cuadrado.removeChild(test)
+//     // cuadrado.removeChild(test)
 
-    // cuadrado.replaceChild(titleSquare,test)
-    // console.log(window.location.href)
+//     // cuadrado.replaceChild(titleSquare,test)
+//     // console.log(window.location.href)
 
-    // location.reload()
+//     // location.reload()
 
-    localStorage.setItem('user',JSON.stringify(escaladores))
-    localStorage.setItem('year',JSON.stringify(year))
+//     localStorage.setItem('user',JSON.stringify(escaladores))
+//     localStorage.setItem('year',JSON.stringify(year))
 
+// })
+
+// getItem.addEventListener('click',() =>{
+//     const getUser = localStorage.getItem('user')
+//     const getYear = localStorage.getItem('year')
+//     console.log(JSON.parse(getUser))
+
+// })
+
+// del.addEventListener('click',() =>{
+//    localStorage.clear()
+// })
+
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////////
+// CLASE 10 Funciones
+////////////////////////////////////////////////////////////////////////////////////
+
+// funcion normal
+
+// function sumar (num1,num2){
+//     // return 'El resultado de la suma es : ' + (num1 + num2);
+//     return `El resultado es: ${num1 + num2}`;
+// }
+
+// const resultado = sumar(10,20);
+// /////////////////////////////////////////////////////////////////////////
+
+// // Funcion anonima
+
+// const anonim = function(color){
+//     var miObjeto ={
+//         color: color
+//     }
+
+//     return miObjeto;
+// }
+
+// ////////////////////////////////////////////////////////////////////////////
+
+// // funcion de flecha
+
+// const mesage = (nombre) => {
+//     return `Hola soy ${nombre}`
+// }
+
+// const response = mesage('Diego');
+
+// //oneLine
+
+// const saludar = nombre => ('Hola Escalador: ' + nombre);
+
+
+// const respuesta = saludar('Ivan');
+
+// const sumar2 = (num1,num2) =>{
+//     const value = 20123;
+//     return num1 + num2;
+    
+// }
+
+// const resultado2 = sumar2(2,3);
+
+// console.log(resultado2);
+
+////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////
+// RECORREDORES DE ARRAYS
+///////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+
+const names = ['Ivan','Emiliano','Richard','Diego'];
+
+const words = ['Bievenmidos','al','curso','de','Web development'];
+
+const numbers = [23,4,12,3,43,13];
+
+const car = {
+    color: 'red',
+    marca: 'Mazda',
+    modelo: 2023
+}
+
+//MAP
+const mapFunction = names.map((names) => {
+    return 'Estudiantes que perteneces al curso de Web Development: ' + names;
 })
 
-getItem.addEventListener('click',() =>{
-    const getUser = localStorage.getItem('user')
-    const getYear = localStorage.getItem('year')
-    console.log(JSON.parse(getUser))
+//FILTER
 
+const filterFunction = names.filter((names) => {
+    return names.includes('a');
 })
 
-del.addEventListener('click',() =>{
-   localStorage.clear()
+//REDUCE
+
+const reduceFunction = words.reduce((acc,item) => {
+    return (acc = acc + ' ' + item);
 })
 
+//FIND
+
+const findFunction = names.find((names) => {
+    return names.includes('a');
+})
+
+//FIND INDEX
+
+const findIndexFunction = numbers.findIndex((number) =>{
+    return number === 43;
+})
+
+//EVERY
+
+const everyFunction = numbers.every((num) =>{
+    return num < 100;
+});
+
+//SOME
+
+const someFunction = names.some((names) =>{
+    return names === 'Luis';
+});
+
+//OBJET KEYS
+
+const key = Object.keys(car);
+
+//OBJET VALUES
+
+const value = Object.values(car);
+
+//OBJECT ENTRIES
+
+const entrie = Object.entries(car);
+console.log(entrie);
